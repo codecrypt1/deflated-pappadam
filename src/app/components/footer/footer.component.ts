c8e1ae08bb60ecbadd0a39dd1d879ac39ce5f066
Hipster16 <100425953+Hipster16@users.noreply.github.com>
2023-11-24 23:06:27 +0530

    Co-authored-by: Rhon <Rhogneld@users.noreply.github.com>

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,11 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'Footer',
+  selector: 'footer-comp',
   standalone: true,
   imports: [CommonModule],
   template: `
-   <div class="w-full"></div>
+   <footer class="font-Montserrat text-black px-2 text-md bg-[#d5ff3f]">
+    <div class="flex justify-between w-full">
+    <a href="https://github.com/Deflated-Pappadam" class="flex justify-between">
+    <img width="20" height="20" src="https://img.icons8.com/material-sharp/24/github.png" alt="github"/>
+      <p>Github</p>
+      </a>
+    <div class="flex justify-between">
+    <img width="22" height="22" src="https://img.icons8.com/ios-filled/50/000000/copyright.png" alt="copyright"/>
+      <p>deflated pappadam</p>
+    </div>
+    <a href="deflatedpappadam@gmail.com" class="flex justify-between">
+      <img width="20" height="20" src="https://img.icons8.com/ios-filled/50/new-post.png" alt="new-post"/>
+      <p>Email</p>
+    </a>
+    </div>
+   </footer>
   `,
   styleUrl: './footer.component.css'
 })
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -4,13 +4,15 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { TagCompComponent } from '../../components/tag-comp/tag-comp.component';
 import { NftBoxComponent } from '../../components/nft-box/nft-box.component';
 import { DropDownComponent } from '../../components/drop-down/drop-down.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
 
 
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule,NavbarComponent,TagCompComponent,NftBoxComponent,DropDownComponent],
+  imports: [CommonModule,NavbarComponent,TagCompComponent,NftBoxComponent,DropDownComponent,FooterComponent],
   schemas: [NO_ERRORS_SCHEMA],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
